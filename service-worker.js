b198db534692565028131f2b603d57243efeb48c
Raisa Sant'Ana <raisa@engearconsultoria.com.br>
2026-07-19 14:22:08 -0300

    Publica e-CAMP v0.58.78

--- service-worker.js
+++ service-worker.js
@@ -8,7 +8,7 @@
  * Todos os caminhos são RELATIVOS, para funcionar no GitHub Pages
  * (https://usuario.github.io/repositorio/) sem ajuste.
  */
-const VERSAO_CACHE = 'ecamp-v0.58.77';
+const VERSAO_CACHE = 'ecamp-v0.58.78';
 
 const ARQUIVOS_APP = [
   './',
